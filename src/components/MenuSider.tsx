41a43c89fc0b6b503d1982e774b1cdfd1285a326
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-08-05 18:33:23 -0300

    feat: created MenuSider component for navigation

---
+++ src/components/MenuSider.tsx
@@ -0,0 +1,15 @@
+const MenuSider = () => {
+  return (
+    <div>
+      MenuSider
+      <ul>
+        <li>Home</li>
+        <li>Education</li>
+        <li>Experiences</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MenuSider
