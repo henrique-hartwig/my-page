38ce2fdf50c33df39d93462aeea79b687d1cdb9a
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-08-05 18:34:00 -0300

    feat: new Header skeleton for user accessibilties

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,17 @@
+import { SunIcon } from "@heroicons/react/24/outline"
+
+const MainHeader = () => {
+  return (
+    <div>
+      MainHeader
+      <header className="m-10 border border-white">
+        CABECALHOaa
+        <button>
+          <SunIcon />
+        </button>
+      </header>
+    </div>
+  )
+}
+
+export default MainHeader
