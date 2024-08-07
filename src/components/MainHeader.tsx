95c9a80a36cc27e22784fea0682846a0f9dc4c24
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-08-06 23:07:13 -0300

    feat: initial styles to Header

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,15 +1,17 @@
+import "./MainHeader.css"
 import { SunIcon } from "@heroicons/react/24/outline"
 
 const MainHeader = () => {
   return (
-    <div>
-      MainHeader
-      <header className="m-10 border border-white">
-        CABECALHOaa
-        <button>
-          <SunIcon />
-        </button>
-      </header>
+    <div className="border border-red w-full h-full flex items-center justify-end gap-24 px-12">
+      My awesome resume page
+      <button>a-</button>
+      <button>A+</button>
+      {/* TODO botar um toogle para mudar entre sol e lua. Talvez fazer um
+       slider que vai alterando gradativamente do modo claro ao escuro */}
+      <button className="s">
+        <SunIcon className="sun w-8 h-8 stroke-yellow-400" />
+      </button>
     </div>
   )
 }
