62a148b63d72229df0dc19d247d945e22a2c9abb
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-08-06 23:06:15 -0300

    feat: set principal grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import "./App.css"
 import MainHeader from "./components/MainHeader"
 import MenuSider from "./components/MenuSider"
 
 function App() {
   return (
-    <>
-      <MainHeader />
-      <MenuSider />
-    </>
+    <div className="page">
+      <div className="header border border-yellow-400">
+        <MainHeader />
+      </div>
+      <div className="aside m-2 border border-red-500">
+        <MenuSider />
+      </div>
+      <div className="content boder border-purple-600">CONTENT</div>
+      <div className="footer boder border border-black">FOOTER</div>
+    </div>
   )
 }
 
