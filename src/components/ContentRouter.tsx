52fe8f3eaac9334d505c1b70d74b93203625501c
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-08-17 10:11:53 -0300

    feat: moved Browser to englobe App and add navigation to some links

--- src/components/ContentRouter.tsx
+++ src/components/ContentRouter.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Education from "./Education"
 import Experiences from "./Experiences"
 import Home from "./Home"
 
 const ContentRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Home />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/experiences" element={<Experiences />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/education" element={<Education />} />
+      <Route path="/experiences" element={<Experiences />} />
+    </Routes>
   )
 }
 
--- src/components/MenuSider.tsx
+++ src/components/MenuSider.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 const MenuSider = () => {
+  const navigate = useNavigate()
+
   return (
     <div className="aside border border-red-500">
       MenuSider
       <ul>
-        <li>Home</li>
-        <li>Education</li>
-        <li>Experiences</li>
-        <li>Contact</li>
+        <li onClick={() => navigate("/")}>Home</li>
+        <li onClick={() => navigate("/education")}>Education</li>
+        <li onClick={() => navigate("/experiences")}>Experiences</li>
+        <li onClick={() => navigate("/contact")}>Contact</li>
       </ul>
     </div>
   )
