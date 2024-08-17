8d2ab6a7b9f37272e0a2f35dd5a3052df06b1d83
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-08-17 15:19:15 -0300

    feat: changes in CSS

--- src/components/MenuSider.tsx
+++ src/components/MenuSider.tsx
@@ -4,9 +4,8 @@ const MenuSider = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="aside border border-red-500">
-      MenuSider
-      <ul>
+    <div className="aside border px-2 border-red-500 py-10 rounded-3xl flex items-start justify-center">
+      <ul className="flex flex-col gap-4 hover:cursor-pointer hover:border">
         <li onClick={() => navigate("/")}>Home</li>
         <li onClick={() => navigate("/education")}>Education</li>
         <li onClick={() => navigate("/experiences")}>Experiences</li>
