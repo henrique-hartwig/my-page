04bd39eb3a05c9c2c63340e711137944bc18e789
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-09-02 12:43:28 -0300

    refactor: create styled component for Li of Menu

--- src/components/MenuSider.tsx
+++ src/components/MenuSider.tsx
@@ -1,20 +1,35 @@
 import { useNavigate } from "react-router-dom"
 
+
+type StyledLiProps = {
+  label: string
+  navigateTo: () => void
+}
+
+const StyledLi = ({ label, navigateTo }: StyledLiProps) => {
+  return (
+    <li
+      className="hover:border hover:rounded-xl hover:shadow-lg hover:scale-110 transition transform duration-200"
+      onClick={navigateTo}
+    >
+      {label}
+    </li>
+  )
+}
+
 const MenuSider = () => {
   const navigate = useNavigate()
 
   return (
     <div className="aside border border-red-500 py-10 rounded-3xl flex items-start justify-center">
       <ul className="flex flex-col gap-4 hover:cursor-pointer items-end">
-        <li
-          className="hover:border hover:rounded-xl hover:shadow-lg hover:scale-110 transition transform duration-200"
-          onClick={() => navigate("/")}
-        >
-          Home
-        </li>
-        <li onClick={() => navigate("/education")}>Education</li>
-        <li onClick={() => navigate("/experiences")}>Experiences</li>
-        <li onClick={() => navigate("/contact")}>Contact</li>
+        <StyledLi label="Home" navigateTo={() => navigate("/")} />
+        <StyledLi label="Education" navigateTo={() => navigate("/education")} />
+        <StyledLi
+          label="Experiences"
+          navigateTo={() => navigate("/experiences")}
+        />
+        <StyledLi label="Contact" navigateTo={() => navigate("/contact")} />
       </ul>
     </div>
   )
