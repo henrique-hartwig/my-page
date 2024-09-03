999deac425b04295bebe82363bc4c39fc1f437c9
henrique-hartwig <hhartwig.geomais@gmail.com>
2024-09-02 22:11:10 -0300

    feat: initial text about education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,7 +1,15 @@
-import React from "react"
-
 const Education = () => {
-  return <div>Education</div>
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div>Educação</div>
+      <section>
+        <header>Curso Técnico integrado ao Ensimo Médio</header>
+        <body>Curso de Eletrônica</body>
+      </section>
+      <section>Graduação</section>
+      <section>Pós-Graduação</section>
+    </div>
+  )
 }
 
 export default Education
