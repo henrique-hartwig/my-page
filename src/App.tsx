bae0a7cb15e2356590402c852b1632ba0d715f0f
henrique.hartwig <henrique2805@gmail.com>
2024-09-03 00:54:35 -0300

    feat: use theme context to change colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css"
-import Header from "./components/Header"
-import MenuSider from "./components/MenuSider"
-
+import { Header, MenuSider } from "./components"
 import Footer from "./components/Footer"
 import RoutesContent from "./components/RoutesContent"
+import { useContext, useEffect } from "react"
+import { ThemeContext } from "./context/ThemeContext"
 
 function App() {
+  const { theme } = useContext(ThemeContext)
+
+  useEffect(() => {
+    document.body.className = theme
+  }, [theme])
+
   return (
     <div className="page border-2">
       <Header />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { useContext } from "react"
 import "./Header.css"
 import { SunIcon } from "@heroicons/react/24/outline"
+import { ThemeContext } from "../context/ThemeContext"
 
-const Header = () => {
+export const Header = () => {
+  const { toggleTheme } = useContext(ThemeContext)
+
   return (
     <div className="header border border-red w-full h-full flex items-center justify-end gap-24 px-12">
       My awesome resume page
@@ -9,11 +13,9 @@ const Header = () => {
       <button>A+</button>
       {/* TODO botar um toogle para mudar entre sol e lua. Talvez fazer um
        slider que vai alterando gradativamente do modo claro ao escuro */}
-      <button className="s">
+      <button className="" onClick={() => toggleTheme()}>
         <SunIcon className="sun w-8 h-8 stroke-yellow-400" />
       </button>
     </div>
   )
 }
-
-export default Header
